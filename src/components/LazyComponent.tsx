118f635d2f31948516193af9acfe65686ab25245
LorgioRoda <lorgiorodaroca@gmail.com>
2024-06-25 08:19:01 +0200

    feat: :sparkles: add LazyComponent

---
+++ src/components/LazyComponent.tsx
@@ -0,0 +1,49 @@
+import { useState, useRef, useEffect, React } from "react";
+
+interface LazySectionProps extends React.HTMLAttributes<HTMLDivElement> {
+  options?: IntersectionOptions;
+}
+
+interface IntersectionOptions {
+  threshold?: number | number[];
+  root?: Element | Document | null;
+  rootMargin?: string;
+}
+
+export const LazyComponent: React.FC<LazySectionProps> = ({ children, options }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const targetRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observerOptions: IntersectionObserverInit = {
+      root: options?.root || null,
+      rootMargin: options?.rootMargin || "0px",
+      threshold: options?.threshold || 0.1,
+    };
+
+    const observerCallback = (entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.disconnect();
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+    if (targetRef.current) {
+      observer.observe(targetRef.current);
+    }
+
+    // Cleanup
+    return () => {
+      if (observer && targetRef.current) {
+        observer.unobserve(targetRef.current);
+        observer.disconnect();
+      }
+    };
+  }, [options]);
+
+  return <div ref={targetRef}>{isVisible ? children : <div>Loading...</div>}</div>;
+};
