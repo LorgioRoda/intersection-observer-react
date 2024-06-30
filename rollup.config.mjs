bda8bd69ee881504a323e69f8bc281b299f789eb
LorgioRoda <lorgiorodaroca@gmail.com>
2024-06-30 12:13:45 +0200

    feat: :arrow_up: add nmew dependency and working on config files

---
+++ rollup.config.mjs
@@ -0,0 +1,18 @@
+// rollup.config.mjs
+import terser from '@rollup/plugin-terser';
+
+export default {
+	input: 'src/main.js',
+	output: [
+		{
+			file: 'bundle.js',
+			format: 'cjs'
+		},
+		{
+			file: 'bundle.min.js',
+			format: 'iife',
+			name: 'version',
+			plugins: [terser()]
+		}
+	],
+};
